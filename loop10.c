f04b15e2d3df5c10e3aec6a0e670d07a1e783327
dy6538643-ai <dy6538643@gmail.com>
2026-09-01 09:12:23 +0530

    Add files via upload

---
+++ loop10.c
@@ -0,0 +1,12 @@
+//Write the program to print the number from 1 to 10 using for loop
+
+#include <stdio.h>
+
+int main() {
+  
+    for(int i =1; i<=10; i++) {
+    
+        printf("%d\n", i);
+    }
+    return 0;
+}
